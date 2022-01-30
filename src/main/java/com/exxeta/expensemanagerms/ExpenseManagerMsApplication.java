130f3c79a98ffd9c6fce0526c8357139c75d93bd
cesc <cscherhoff@web.de>
2022-01-30 18:25:57 +0100

    Initial commit

---
+++ src/main/java/com/exxeta/expensemanagerms/ExpenseManagerMsApplication.java
@@ -0,0 +1,13 @@
+package com.exxeta.expensemanagerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagerMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exxeta/expensemanagerms/ExpenseManagerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exxeta.expensemanagerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
