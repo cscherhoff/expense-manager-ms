0490968cfd3bf0812823237f159b16cea74e4d67
cesc <cscherhoff@web.de>
2022-04-02 21:58:32 +0200

    Bind categories and articles to the user id

---
+++ src/main/java/com/exxeta/expensemanagerms/controller/expenseController/CategoryController.java
@@ -0,0 +1,32 @@
+package com.exxeta.expensemanagerms.controller.expenseController;
+
+import com.exxeta.expenseservice.services.CategoryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController()
+@RequestMapping(path = "/categories/user/{userId}")
+public class CategoryController {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public String getAllCategories(@PathVariable long userId) {
+        try {
+            return mapper.writeValueAsString(categoryService.getAllCategories(userId));
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+    }
+}
